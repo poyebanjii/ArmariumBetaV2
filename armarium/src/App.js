1d2b90db5484fcb38111ed4ff581e653d4619c02
Tristan <ricemaking69@gmail.com>
2024-02-23 10:21:23 -0600

    added in styling for the login family of pages

--- armarium/src/App.js
+++ armarium/src/App.js
@@ -39,7 +39,7 @@ function App() {
 
   return (
       <div className="App">
-        <h2>Armarium</h2>
+        <h2>ARMARIUM</h2>
         <label>
           Email:
           <input
--- armarium/src/Register.js
+++ armarium/src/Register.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import './Register.css';
 
 /**
  * The Register page where users can create their accounts.
